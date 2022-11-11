1d8452c38d619d07fa8a039054b069e301ab5656
Difort12 <darell12timotius@gmail.com>
2022-11-11 09:13:32 +0700

    Schoolyte

---
+++ Edco-API/storage/framework/views/a53e4fd14171561c2376c9a08c3834b6cdf8e87f.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    "Hello"
+</html>
+<?php /**PATH C:\Users\Darel\Desktop\Edco\Edco-API\resources\views/welcome.blade.php ENDPATH**/ ?>
