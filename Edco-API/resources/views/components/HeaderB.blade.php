4f16c3eeafc6e3253407b084ea64b1c8b442c47b
Schoolyte <darell12timotius@gmail.com>
2022-11-15 15:46:58 +0700

    Schoolyte

--- Edco-API/resources/views/components/HeaderB.blade.php
+++ Edco-API/resources/views/components/HeaderB.blade.php
@@ -6,15 +6,16 @@
 <head>
     <link href="Style/NavbarB.css" rel="stylesheet">
 </head>
+
 <nav class="navbar">
     <div class="navbar navbar-expand-lg">
         <div class="container-fluidB">
         <a class="navbar-brand" href="#">
-        <img src="assets/logo1.png" className="Logo-img "  alt="logo" />
+        <img src="assets/logo1.png" className="Logo-img"  alt="logo" />
         </a>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="menu-beranda">
-            <li><a class="nav-brd" href="#">Beranda</a></li>
+            <li><a class="nav-brd" href="/">Beranda</a></li>
             <li><a class="nav-brd" href="#">Kontak</a></li>
             <button type="button" class="btn-login">Login</button>
             </ul>
