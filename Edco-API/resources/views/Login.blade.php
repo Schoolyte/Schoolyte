78aaca0a59fd3bd7682691dc348723d718a2db18
Schoolyte <darell12timotius@gmail.com>
2022-11-15 11:02:21 +0700

    Schoolyte

--- Edco-API/resources/views/Login.blade.php
+++ Edco-API/resources/views/Login.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Login Page</title>
+        <title>Login Page - Schoolyte</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Glory:wght@800&display=swap');">
@@ -19,21 +19,21 @@
             <body>
                 <img src="assets/logo.png" class="logo-img" alt="logo-Scoolyte">
 
-                <form class="LoginForm">
-
+                <form  action="{{route('postlogin')}}" method="post" class="LoginForm">
+                    {{ csrf_field() }}
                     <div class="title">
                         LOGIN
                     </div>
 
-                    <div class="label">
+                    <div  class="label">
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label-ID">ID/NIS/NIP</label>
-                        <input type="email" class="form-controlx" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan ID/NIS/NIP anda">
+                        <input type="email" name="id" class="form-controlx" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan ID/NIS/NIP anda">
                         <div id="emailHelp" class="form-text" ></div>
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label-PW">Password</label>
-                        <input type="password" class="form-controly" id="exampleInputPassword1" maxlength="8"  placeholder="Masukkan password anda">
+                        <input type="password" name="password" class="form-controly" id="exampleInputPassword1" maxlength="8"  placeholder="Masukkan password anda">
                         <div id="emailHelp" class="form-text" ></div>
                     </div>
                     </div>
--- Edco-API/routes/web.php
+++ Edco-API/routes/web.php
@@ -2,21 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-
-
-Route::get('/Login', function () {
-    return view('Login');
+Route::get('/', function () {
+    return view('Beranda');
 });
+
+Route::get('login', function () {
+    return view('login');
+});
+
+
+Route::post('/postlogin', 'LoginController@postlogin')->name('postlogin');
+
+
+
 
 
